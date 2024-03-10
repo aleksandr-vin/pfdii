e1c76a8c6de93688de6383196b3d127c93014955
Aleksandr Vinokurov <aleksandr.vin@gmail.com>
2024-03-10 15:26:34 +0100

    add PreLoader to boot in EFI with Secure Boot

--- KNOWN-PROBLEMS.md
+++ KNOWN-PROBLEMS.md
@@ -4,3 +4,14 @@
 
 Docker Desktop before version 4.27.1 has a bug and *setup-syslinux.sh* will fail complaining
 about unknown 'vfat' filesystem.
+
+## EFI Secure Boot
+
+At the first boot with Secure Boot enabled:
+
+> 1. PreLoader should launch, but it will probably complain that it couldn't launch loader.efi.
+>    It will then launch HashTool, which is the program that PreLoader uses to store information (hashes) on the programs you authorize.
+> 2. In HashTool, select the Enroll Hash option.
+> 3. Browse to EFI/syslinux and select the loader.efi program file. HashTool asks for confirmation; respond Yes.
+
+More info on http://www.rodsbooks.com/efi-bootloaders/secureboot.html#preloader
--- setup-syslinux.sh
+++ setup-syslinux.sh
@@ -34,8 +34,27 @@ DST="/mnt/EFI/BOOT"
 mkdir -p "${DST}"
 cp -r /usr/lib/syslinux/efi64/* "${DST}"
 
-# Placing syslinux.efi under fallback name instead of calling efibootmgr:
-cp -r /usr/lib/syslinux/efi64/syslinux.efi "${DST}"/bootx64.efi
+# Placing syslinux.efi under loader.eif,
+# see Rodsbooks' Secure Boot, http://www.rodsbooks.com/efi-bootloaders/secureboot.html#preloader.
+#
+cp /usr/lib/syslinux/efi64/syslinux.efi "${DST}"/loader.efi
+cp /preloader-signed/{PreLoader,HashTool}.efi "${DST}"/
+cp /preloader-signed/PreLoader.efi "${DST}"/bootx64.efi # make PreLoader the default one
+cat <<EOF
+#
+#
+#   At the first boot with Secure Boot enabled:
+#
+#   > 1. PreLoader should launch, but it will probably complain that it couldn't launch loader.efi.
+#   >    It will then launch HashTool, which is the program that PreLoader uses to store information (hashes) on the programs you authorize.
+#   > 2. In HashTool, select the Enroll Hash option.
+#   > 3. Browse to EFI/syslinux and select the loader.efi program file. HashTool asks for confirmation; respond Yes.
+#
+#   More info on http://www.rodsbooks.com/efi-bootloaders/secureboot.html#preloader
+#
+#
+EOF
+#
 
 #efibootmgr --create --disk /dev/"${loop_dev:0:-2}" \
 #	   --part 1 --loader /EFI/syslinux/syslinux.efi --label "Syslinux" --unicode
